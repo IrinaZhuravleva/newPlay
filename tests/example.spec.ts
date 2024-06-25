b1d23e8278038723b3ff09f7dc067c17d63a1867
Irina Zhuravleva <iravizion@gmail.com>
2024-06-25 10:53:11 +0100

    Check sign in is added

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,3 +5,27 @@ test('has title', async ({ page }) => {
 
   await expect(page).toHaveTitle(/CodeMetrics/);
 });
+
+test('check sign up', async({page}) => {
+  const signUpButton = page.getByRole("link", { name: "Sign up" });
+  const sighUpHeading = page.getByRole("heading", { name: "Sign up" });
+
+  await page.goto('https://codemetrics.dev/');
+
+  await signUpButton.waitFor();
+  await signUpButton.click();
+  await sighUpHeading.waitFor();
+  await expect(sighUpHeading).toBeVisible();
+})
+
+test("check sign in", async ({ page }) => {
+  const signInButton = page.getByRole("link", { name: "Sign in" });
+  const sighInHeading = page.getByRole("heading", { name: "Sign in" });
+
+  await page.goto("https://codemetrics.dev/");
+
+  await signInButton.waitFor();
+  await signInButton.click();
+  await sighInHeading.waitFor();
+  await expect(sighInHeading).toBeVisible();
+});
